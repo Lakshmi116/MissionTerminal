2d2c1d15d7674f66ad34fa5d20db891c8d693118
Lakshmi116 <lakshminarayana822002@gmail.com>
2022-09-08 16:42:58 +0530

    new csv

--- new.py
+++ new.py
@@ -1,23 +1,45 @@
-from tracemalloc import start
+import math
 
 
-round = 7
-round = int(input("Round: "))
-start = int(input("Start: "))
-use = int(input("Use: "))
-# use = 3
+# rnd = 7
+# rnd = int(input("Round: "))
+# # start = int(input("Start: "))
+# use = int(input("Use: "))
+# # use = 3
 
 
 
-def mp(begin, use, round):
+def mp(begin, use, rnd):
     if(use>=begin):
-        return round
-    return (begin-use)*0.75 + round 
+        return rnd
+    return (begin-use)*0.75 + rnd 
+# start = []
+# for i in range(10):
+#     start.append(i+1)
+# for i in range(10):
+#     for st in range(len(start)):
+#         next = mp(start[st], use, rnd)
+#         k = round(next, 1)
+#         print(k, end=" ")
+#         start[st] = next
+#     print()
 
+out = open("new.csv")
 
-for i in range(20):
-    next = mp(start, use, round)
-    print(i+1, next)
-    start = next
+for rnd in range(5,10):
+    for use in range(rnd+1):
+        start = []
+        for i in range(21):
+            start.append(i+1)
+        for i in range(10):
+            for st in range(len(start)):
+                    next = mp(start[st], use, rnd)
+                    k = round(next, 1)
+                    tmp = str(i) + ", "+str(round)+", "+str(use)+", "+str(start)+", "+str(k)+"\n"
+                    # out.write(tmp)
+                    out.write("")
+                    start[st] = next 
+
+        
 
     
